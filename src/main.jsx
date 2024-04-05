7302e6829a19627109ceb49f5fd9ed44fea52b9a
Hamid Eslmai <hamideslami71@gmail.com>
2024-04-05 11:12:24 +0330

    save Todo in LocalStorage

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
+import { PersistGate } from 'redux-persist/integration/react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { store } from './store/store'
+import { store, persistor } from './store/store'
 import { Provider } from 'react-redux'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
     <>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
       <ToastContainer />
     </Provider>
       
--- src/store/store.jsx
+++ src/store/store.jsx
@@ -1,8 +1,27 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import todosReducer from './slices/todoSlice'
 
-export const store = configureStore({
-  reducer: {
-    todos: todosReducer,
-  },
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
+ 
+// import rootReducer from './reducers'
+
+const rootReducer = combineReducers({
+  todos: todosReducer,
 })
+ 
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['todos'],
+}
+ 
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+const store = configureStore({
+  reducer: persistedReducer,
+})
+
+let persistor = persistStore(store)
+
+export { store, persistor }
